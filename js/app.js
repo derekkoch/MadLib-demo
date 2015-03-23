072c18d8dd6cee56c9d1717e5165b51c76fde94e
Christopher Impastato <Chris@Christophers-MacBook-Pro.local>
2015-03-23 16:54:48 -0400

    Animate. Add styling with jQuery.

--- js/app.js
+++ js/app.js
@@ -2,14 +2,15 @@ $(document).ready(function() {
 
   $("#submit-btn").click(function(){
 
-    var exclamation = $("#exclamation").val();
+  	var exclamation = $("#exclamation").val();
     var adverb = $("#adverb").val();
     var noun = $("#noun").val();
     var verb = $("#verb").val();
 
     var mad_lib = "'" + exclamation + "!' he said, as he " + adverb + " jumped onto his " + noun + " and " + verb + " into the sunset.";
 
-    console.log(mad_lib);
+    $("#mad_lib").hide();
+    $("#mad_lib").text(mad_lib).fadeIn("slow").css("color","#0000ff");
 
     });
 });
