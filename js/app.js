70c623741fb37b2f59f64de7c25eda34ae19ffd4
Christopher Impastato <Chris@Christophers-MacBook-Pro.local>
2015-03-23 16:39:53 -0400

    Create variables.

--- js/app.js
+++ js/app.js
@@ -2,7 +2,14 @@ $(document).ready(function() {
 
   $("#submit-btn").click(function(){
 
-    console.log('Hello, world!');
+    var exclamation = $("#exclamation").val();
+    var adverb = $("#adverb").val();
+    var noun = $("#noun").val();
+    var verb = $("#verb").val();
+
+    var mad_lib = "'" + exclamation + "!' he said, as he " + adverb + " jumped onto his " + noun + " and " + verb + " into the sunset.";
+
+    console.log(mad_lib);
 
     });
 });
